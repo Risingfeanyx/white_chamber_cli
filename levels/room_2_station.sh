b6b5f03bec882d2ffb9cb369fcddc700892e4f60
Risingfeanyx <35235934+Risingfeanyx@users.noreply.github.com>
2020-08-05 03:36:47 -0400

    Update room_2_station.sh

--- levels/room_2_station.sh
+++ levels/room_2_station.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-	{
-	clear
-	RED='\033[0;31m'
-	NC='\033[0m' # No Color
-	#mpv https://youtu.be/iwoAFu8Y_8U --no-video > /dev/null 2>&1&
-	#mpv music mpv music/02.\ Station\ Ambience.mp3 --no-video > /dev/null 2>&1&
-	clear ; sleep 5 ; echo "As the silver door slides away, the flourescent lights lights flicker and hum to life."
-	sleep5 ; echo "Your eyes adjust to the light, and see what appears to be a common room; at the center is a holographic console. But what
-	captures your vision before all else are the red visceral stains all over the floor and walls. The red appears almost caked to the walls, as 
-	if it had been here over the course of decasdes, frozen in time."
+    {
+    clear
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+    #mpv https://youtu.be/iwoAFu8Y_8U --no-video > /dev/null 2>&1& mpv music mpv music/02.\ Station\ Ambience.mp3 --no-video > /dev/null 2>&1&
+    clear ; sleep 5 ; echo "As the silver door slides away, the flourescent lights lights flicker and hum to life."
+    sleep 5 ; echo "Your eyes adjust to the light, and see what appears to be a common room; at the center is a holographic console. But what
+    captures your vision before all else are the red visceral stains all over the floor and walls. The red appears almost caked to the walls, as
+    if it had been here over the course of decasdes, frozen in time."
+    sleep 5 ; echo "In the center of the room, there appears to be a wide computer console, powered off, and dimly illuminated by dim crimson emergency lights."
+    echo "To your right and left are sleek silver doors"
+
